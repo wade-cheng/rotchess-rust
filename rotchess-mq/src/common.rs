300ff10a75b3165f48e4172cb8340e381bf4cadf
wade-cheng <wadecheng3301@gmail.com>
2025-08-05 13:17:13 -0700

    create common and move over conf

--- rotchess-mq/src/common.rs
+++ rotchess-mq/src/common.rs
@@ -1,8 +1,9 @@
 use macroquad::miniquad::conf::Icon;
 use macroquad::prelude::ImageFormat;
 use macroquad::texture::Image;
+use macroquad::window::Conf;
 
-/// Gets the icon for the game.
+/// Generate the icon for the game.
 ///
 /// Implementation uses a macro because reading an image via macroquad requires a macro, so unfortunately something like
 ///
@@ -26,7 +27,7 @@ use macroquad::texture::Image;
 /// ```
 ///
 /// does not work.
-pub fn rotchess_icon() -> Icon {
+fn rotchess_icon() -> Icon {
     macro_rules! icon_from_path {
         ($path:expr) => {
             Image::from_file_with_format(include_bytes!($path), Some(ImageFormat::Png))
@@ -45,3 +46,26 @@ pub fn rotchess_icon() -> Icon {
 
     Icon { small, medium, big }
 }
+
+/// Generate the window config for the game.
+///
+/// This should only be used literally once, so we don't bother `LazyLock`ing it under the hood.
+pub fn window_conf() -> Conf {
+    Conf {
+        window_title: String::from("Rotating Chess"),
+        window_height: 400,
+        window_width: 400,
+        icon: Some(rotchess_icon()),
+        sample_count: 0, // remove antialiasing
+        high_dpi: true,
+        ..Default::default()
+    }
+}
+
+// use std::sync::LazyLock;
+// pub fn font() -> &'static Font {
+//     static FONT: LazyLock<Font> = LazyLock::new(|| {
+//         macroquad::text::load_ttf_font_from_bytes(include_bytes!("../assets/OpenSans.ttf")).unwrap()
+//     });
+//     &FONT
+// }
--- rotchess-mq/src/main.rs
+++ rotchess-mq/src/main.rs
@@ -1,19 +1,7 @@
 use macroquad::prelude::*;
 
 use rotchess_mq::app::App;
-use rotchess_mq::common;
-
-fn window_conf() -> Conf {
-    Conf {
-        window_title: String::from("Rotating Chess"),
-        window_height: 400,
-        window_width: 400,
-        icon: Some(common::rotchess_icon()),
-        sample_count: 0, // remove antialiasing
-        high_dpi: true,
-        ..Default::default()
-    }
-}
+use rotchess_mq::common::window_conf;
 
 #[macroquad::main(window_conf)]
 async fn main() {
