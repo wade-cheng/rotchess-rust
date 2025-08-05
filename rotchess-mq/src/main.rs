f60043e65a42f76f19db79aae74428eb957a9af2
wade-cheng <wadecheng3301@gmail.com>
2025-08-05 13:32:33 -0700

    remove unused App in favor of direct Ui

--- rotchess-mq/src/main.rs
+++ rotchess-mq/src/main.rs
@@ -1,11 +1,11 @@
 use macroquad::prelude::*;
 
-use rotchess_mq::app::App;
 use rotchess_mq::common::window_conf;
+use rotchess_ui::Ui;
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    let mut app = App::new();
+    let mut app = Ui::new();
 
     loop {
         app.update();
